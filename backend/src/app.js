dc44b28aa1817475a906119cff55317775f30b0d
Quynh Chau <quynhchaun9@gmail.com>
2026-06-07 16:43:46 +0700

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,8 @@ const app = express();
 
 const allowedOrigins = [
   process.env.CLIENT_URL,
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
   "http://localhost:5174",
   "http://127.0.0.1:5174",
 ].filter(Boolean);
