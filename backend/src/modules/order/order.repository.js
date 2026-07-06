6e18665c60aec52207fb6d23a46ff1a02f78aef2
Vhoang-MK <trancong25112004@gmail.com>
2026-07-06 20:01:16 +0700

    feat(order): update order constants, controller, repository and service

--- backend/src/modules/order/order.repository.js
+++ backend/src/modules/order/order.repository.js
@@ -49,6 +49,7 @@ const orderRepository = {
     const [items, total] = await Promise.all([
       Order.find(filter)
         .populate("items.foodItemId", "name") // Fix #7: populate để có tên món trong response
+        .populate("staffId", "fullName username role")
         .sort({ createdAt: -1 })
         .skip(skip)
         .limit(limit),
@@ -59,10 +60,9 @@ const orderRepository = {
   },
 
   async findById(id) {
-    return Order.findOne({ _id: toObjectId(id) }).populate(
-      "items.foodItemId",
-      "name",
-    );
+    return Order.findOne({ _id: toObjectId(id) })
+      .populate("items.foodItemId", "name")
+      .populate("staffId", "fullName username role");
   },
 
   async updateStatusById(id, status) {
